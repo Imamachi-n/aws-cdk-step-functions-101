bf580970e0d7435116d2957434ae78832dbc74cc
Imamachi-n <naoto.imamachi@gmail.com>
2020-09-02 09:53:42 +0900

    feat: 並列処理のStep Functionsを追加

--- src/task.ts
+++ src/task.ts
@@ -3,11 +3,34 @@ type TaskResponse = {
   message: string;
 };
 
-export async function handler(event: any): Promise<TaskResponse> {
+export async function basicLambdaFn(event: any): Promise<TaskResponse> {
   console.log(JSON.stringify(event));
 
   return {
     title: "ようこそ！",
     message: "Step Functions の世界へ！",
+  };
+}
+
+export async function parallel1LambdaFn(event: any): Promise<TaskResponse> {
+  console.log(JSON.stringify(event));
+
+  return {
+    title: "並列処理",
+    message: "処理１を実行しました。",
+  };
+}
+export async function parallel2LambdaFn(event: any): Promise<TaskResponse> {
+  console.log(JSON.stringify(event));
+
+  return {
+    title: "並列処理",
+    message: "処理２を実行しました。",
+  };
+}
+
+export async function finalLambdaFn(event: any): Promise<object> {
+  return {
+    response: event,
   };
 }
--- stacks/StepFunctions.ts
+++ stacks/StepFunctions.ts
@@ -7,10 +7,17 @@ export class StepFunctionsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const taskFn = new lambda.Function(this, "taskFn", {
+    this.basicSfn();
+    this.parallelSfn();
+  }
+
+  // シーケンシャルステップを持つ基本的な Step Functions
+  // MEMO: task の名前も含めてユニークな名前にする必要がある。
+  private basicSfn() {
+    const taskFn = new lambda.Function(this, "basicLambdaFn", {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.fromAsset("dist/src"),
-      handler: "task.handler",
+      handler: "task.basicLambdaFn",
     });
 
     const firstState = new tasks.LambdaInvoke(this, "最初の処理", {
@@ -22,10 +29,49 @@ export class StepFunctionsStack extends cdk.Stack {
       lambdaFunction: taskFn,
     });
 
-    const definition = firstState.next(secondState);
+    const basicDefinition = firstState.next(secondState);
 
     new sfn.StateMachine(this, "stateMachine", {
-      definition: definition,
+      definition: basicDefinition,
+    });
+  }
+
+  // 並列処理を使った Step Functions
+  private parallelSfn() {
+    const parallel1taskFn = new lambda.Function(this, "parallel1LambdaFn", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("dist/src"),
+      handler: "task.parallel1LambdaFn",
+    });
+    const parallel2taskFn = new lambda.Function(this, "parallel2LambdaFn", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("dist/src"),
+      handler: "task.parallel2LambdaFn",
+    });
+    const finalTaskFn = new lambda.Function(this, "finalLambdaFn", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("dist/src"),
+      handler: "task.finalLambdaFn",
+    });
+
+    const parallelState1 = new tasks.LambdaInvoke(this, "並列処理１", {
+      lambdaFunction: parallel1taskFn,
+      outputPath: "$.Payload",
+    });
+    const parallelState2 = new tasks.LambdaInvoke(this, "並列処理２", {
+      lambdaFunction: parallel2taskFn,
+      outputPath: "$.Payload",
+    });
+    const finalState = new tasks.LambdaInvoke(this, "最後の処理", {
+      inputPath: "$",
+      lambdaFunction: finalTaskFn,
+    });
+
+    const parallelDefinition = new sfn.Parallel(this, "parallelTasks");
+    parallelDefinition.branch(parallelState1, parallelState2).next(finalState);
+
+    new sfn.StateMachine(this, "parallelStateMachine", {
+      definition: parallelDefinition,
     });
   }
 }
